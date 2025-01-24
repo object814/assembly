66f0688114715aa2a7bf9a45da894a065cad559b
isabella4444x <isabella4444x@sjtu.edu.cn>
2025-01-24 20:04:51 +0800

    update

--- Realsense_camera.py
+++ Realsense_camera.py
@@ -1,8 +1,12 @@
 import sys
 
 sys.path.append(".")
+sys.path.append("/home/shaol/data/zhoujx/rw/third_party/xarm6")
+
 import os
 
+import matplotlib.pyplot as plt
+import cv2
 os.environ["TORCH_CUDNN_SDPA_ENABLED"] = "1"
 from xarm6_interface.utils.sam_prompt_drawer import SAMPromptDrawer, SAM2PromptDrawer, shrink_mask
 from xarm6_interface.utils.realsense import MultiRealsense, get_masked_pointcloud, remove_outliers
@@ -27,34 +31,53 @@ angle_in_radians = math.radians(angle_in_degrees)
 class Realtime_PC:
 
     def __init__(self):
-        self.prompt_drawer = SAM2PromptDrawer(window_name="Prompt Drawer",
-                                              screen_scale=2.0,
-                                              sam_checkpoint=SAM_PATH,
-                                              device="cuda",
-                                              model_type=SAM_TYPE)
 
-        arm_right_cam_serial = "241122074374"  # 241122074374, 233622079809
-        # top_cam_serial = '233622079809'
-        camera_serial_nums = [arm_right_cam_serial]
+        # self.prompt_drawer = SAM2PromptDrawer(window_name="Prompt Drawer",
+        #                                       screen_scale=2.0,
+        #                                       sam_checkpoint=SAM_PATH,
+        #                                       device="cuda",
+        #                                       model_type=SAM_TYPE)
+
+        self.prompt_drawer = SAMPromptDrawer(window_name="Prompt Drawer",
+                                    screen_scale=2.0,
+                                    sam_checkpoint=SAM_PATH,
+                                    device="cuda",
+                                    model_type=SAM_TYPE)
+
+        front_cam_serial = "317222074181"  # 241122074374, 233622079809
+        exp_name = "0124_excalib_capture00"
+        left_exp_name = "0124_excalib_capture00_Left"
+        # camera_serial_nums = [arm_right_cam_serial]
+        camera_serial_nums = [front_cam_serial]
         # camera_serial_nums = [top_cam_serial]
         self.multi_rs = MultiRealsense(camera_serial_nums)
-        # arm_right_cam_K_path = Path("3rdparty/xarm6/data/camera/mounted_white/K.npy")
-        arm_right_cam_K_path = Path(f"3rdparty/xarm6/data/camera/{arm_right_cam_serial}/K.npy")
-        # arm_right_cam_K_path = Path("3rdparty/xarm6/data/camera/mounted_top/K.npy")
-        arm_right_cam_K = np.load(arm_right_cam_K_path)
-        # arm_right_cam_X_BaseCamera_path = Path("3rdparty/xarm6/data/camera/mounted_white/0922_excalib_capture00/optimized_X_BaseCamera.npy")
-        arm_right_cam_X_BaseCamera_path = Path(
-            f"3rdparty/xarm6/data/camera/{arm_right_cam_serial}/1113_excalib_capture02/optimized_X_BaseCamera.npy")
-        # arm_right_cam_X_BaseCamera_path = Path("3rdparty/xarm6/data/camera/mounted_top/1109_excalib_capture00/optimized_X_BaseCamera.npy")
-        arm_right_cam_X_BaseCamera = np.load(arm_right_cam_X_BaseCamera_path)
-        self.multi_rs.set_intrinsics(0, arm_right_cam_K[0, 0], arm_right_cam_K[1, 1], arm_right_cam_K[0, 2],
-                                     arm_right_cam_K[1, 2])
-        self.camera_wxyzs = [
-            R.from_matrix(arm_right_cam_X_BaseCamera[:3, :3]).as_quat()[[3, 0, 1, 2]],
-        ]
-        self.camera_positions = [arm_right_cam_X_BaseCamera[:3, 3]]
-        self.X_BaseCamera_list = [arm_right_cam_X_BaseCamera]
 
+        # arm_right_cam_K_path = Path(f"3rdparty/xarm6/data/camera/{arm_right_cam_serial}/K.npy")
+        front_cam_K_path = Path(f"third_party/xarm6/data/camera/{front_cam_serial}/K.npy")
+
+        # arm_right_cam_K = np.load(arm_right_cam_K_path)
+        front_cam_K = np.load(front_cam_K_path)
+        
+        
+        front_cam_X_BaseCamera_path = Path(f"third_party/xarm6/data/camera/{front_cam_serial}/{exp_name}/optimized_X_BaseCamera.npy")
+        Left_front_cam_X_BaseCamera_path = Path(f"third_party/xarm6/data/camera/{front_cam_serial}/{left_exp_name}/optimized_X_BaseCamera.npy")
+        
+        front_cam_X_BaseCamera = np.load(front_cam_X_BaseCamera_path)
+
+        self.multi_rs.set_intrinsics(0, front_cam_K[0, 0], front_cam_K[1, 1], front_cam_K[0, 2],
+                                     front_cam_K[1, 2])
+        self.camera_wxyzs = [
+            R.from_matrix(front_cam_X_BaseCamera[:3, :3]).as_quat()[[3, 0, 1, 2]],
+        ]
+        self.camera_positions = [front_cam_X_BaseCamera[:3, 3]]
+        self.X_BaseCamera_list = [front_cam_X_BaseCamera]
+        self.Extrinsic_matrix = front_cam_X_BaseCamera
+        self.Intrinsic_matrix = front_cam_K
+        self.camera_serial = front_cam_serial
+        self.camera_name = "front_cam"
+        self.camera_id = 0
+        self.camera_type = "realsense"
+        self.camera_wxyz = R.from_matrix(front_cam_X_BaseCamera[:3, :3]).as_quat()[[3, 0, 1, 2]]
         self.X_BaserightBaseleft = np.array([[-0.9994789, -0.02021456, -0.02516249, 1.1056788],
                                              [0.02050998, -0.99972314, -0.01153819, -0.03005717],
                                              [-0.02492228, -0.01204827, 0.9996168, 0.00512242], [0.0, 0.0, 0.0, 1.0]])
@@ -85,8 +108,8 @@ class Realtime_PC:
                                       position=(self.X_BaserightBaseleft[:3, 3]))
 
         self.sv.scene.add_frame("camera",
-                                wxyz=R.from_matrix(arm_right_cam_X_BaseCamera[:3, :3]).as_quat()[[3, 0, 1, 2]],
-                                position=arm_right_cam_X_BaseCamera[:3, 3])
+                                wxyz=R.from_matrix(front_cam_X_BaseCamera[:3, :3]).as_quat()[[3, 0, 1, 2]],
+                                position=front_cam_X_BaseCamera[:3, 3])
         self.sv.scene.add_frame("left",
                                 wxyz=R.from_matrix(self.X_BaserightBaseleft[:3, :3]).as_quat()[[3, 0, 1, 2]],
                                 position=self.X_BaserightBaseleft[:3, 3])
@@ -108,12 +131,13 @@ class Realtime_PC:
             # pc_o3d = rtr_dict["pointcloud_o3d"]
 
             self.prompt_drawer.reset()
-            if self.if_init:
-                mask_np = self.prompt_drawer.run(rgb)  # (720, 1280)
-                self.if_init = False
-            else:
-                mask_np = self.prompt_drawer.track(rgb)
-            # mask_np = shrink_mask(mask_np, shrink_coefficient=0.9)
+            mask_np = self.prompt_drawer.run(rgb) 
+            # if self.if_init:
+            #     mask_np = self.prompt_drawer.run(rgb)  # (720, 1280)
+            #     self.if_init = False
+            # else:
+            #     mask_np = self.prompt_drawer.track(rgb)
+            mask_np = shrink_mask(mask_np, shrink_coefficient=0.9)
             h, w = mask_np.shape[-2:]
 
             masked_pc_o3d = get_masked_pointcloud(rgb, depth, mask_np.reshape(h, w),
@@ -126,8 +150,13 @@ class Realtime_PC:
             self.masked_pc_o3d_W.points = o3d.utility.Vector3dVector(rs_pc_in_B)
             self.masked_pc_o3d_W.colors = masked_pc_o3d.colors
 
-            self.vis_PC()
-            return self.masked_pc_o3d_W
+            self.rgb = rgb
+            self.depth = depth
+            self.K = self.multi_rs.camera_data[camera_idx]["pinhole_camera_intrinsic"]
+    def get_masked_pc(self):
+
+        self.vis_PC()
+        return self.masked_pc_o3d_W
 
     def get_sv(self):
         return self.sv
@@ -149,6 +178,7 @@ class Realtime_PC:
     def load_pc(self, load_dir, file_name):
         load_path = os.path.join(load_dir, file_name)
         self.masked_pc_o3d_W = o3d.io.read_point_cloud(load_path)
+        return self.masked_pc_o3d_W 
 
     def get_3d_points_on_object(self, pixel_coords, object_mask, camera_idx=0):
         """
@@ -227,85 +257,96 @@ class Realtime_PC:
 
         # 可视化
         o3d.visualization.draw_geometries([object_pcd, highlight_pcd], window_name="2D -> 3D 点匹配可视化（物体点云）")
+    
+    def get_3d_point_from_2d(self, u, v):
+        """
+        根据 2D 像素坐标 (u, v)，获取对应点云中的 3D 坐标。
+        :param u: 像素列坐标 (x)
+        :param v: 像素行坐标 (y)
+        :return: 对应的 3D 点 (X, Y, Z)
+        """
+        # 提取深度图和内参矩阵
+        depth = self.multi_rs.camera_data[0]["depth"]
+        K = self.multi_rs.camera_data[0]["pinhole_camera_intrinsic"].intrinsic_matrix
 
+        # 获取相机内参
+        fx, fy = K[0, 0], K[1, 1]
+        cx, cy = K[0, 2], K[1, 2]
+        print(f"fx: {fx}, fy: {fy}, cx: {cx}, cy: {cy}")
+        # 深度值 Z
+        Z = depth[int(v), int(u)]/ 1000.0
+        if Z == 0:
+            raise ValueError(f"No valid depth value at pixel ({u}, {v})")
 
+        # 计算 3D 坐标
+        X = (u - cx) * Z / fx
+        Y = (v - cy) * Z / fy
+        # 打印 3D 点和点云范围
+        print(f"Generated 3D point: [{X}, {Y}, {Z}]")
+        print("Point cloud bounds:")
+        print(self.masked_pc_o3d_W.get_min_bound(), self.masked_pc_o3d_W.get_max_bound())
+        return np.array([X, Y, Z])
+    def visualize_2d_and_3d(self, u, v):
+        """
+        可视化 RGB 图像中的 2D 点和点云中的对应 3D 点
+        :param u: 像素列坐标 (x)
+        :param v: 像素行坐标 (y)
+        """
+        # 获取 3D 点
+        point_3d_cam = self.get_3d_point_from_2d(u, v)
+        point_3d = self.transform_to_world(point_3d_cam)
+        # 在 RGB 图像上标记 2D 点
+        img_with_point = self.rgb.copy()
+        cv2.circle(img_with_point, (int(u), int(v)), radius=5, color=(0, 0, 255), thickness=-1)
+        plt.imshow(cv2.cvtColor(img_with_point, cv2.COLOR_BGR2RGB))
+        plt.title(f"2D Point: ({u}, {v})")
+        plt.axis("off")
+        plt.show()
+
+        # 在点云中高亮 3D 点
+        single_point_cloud = o3d.geometry.PointCloud()
+        single_point_cloud.points = o3d.utility.Vector3dVector([point_3d])
+        single_point_cloud.paint_uniform_color([0, 1, 0])  # green高亮点
+
+        # 合并点云
+        combined_point_cloud = self.masked_pc_o3d_W + single_point_cloud
+        o3d.visualization.draw_geometries(
+            [combined_point_cloud],
+            window_name="3D Visualization",
+            point_show_normal=False,
+        )
+    def transform_to_world(self, point_3d):
+        """
+        将 3D 点从相机坐标系转换到点云的世界坐标系。
+        :param point_3d: 相机坐标系下的 3D 点 (X, Y, Z)
+        :return: 世界坐标系下的 3D 点
+        """
+        # 获取外参矩阵
+        extrinsic = self.Extrinsic_matrix
+        # 转换为齐次坐标
+        point_camera = np.array([point_3d[0], point_3d[1], point_3d[2], 1.0])  # 添加齐次维度
+
+        # 应用外参变换
+        point_world = extrinsic @ point_camera
+
+        return point_world[:3]  
 if __name__ == "__main__":
     realtime_PC = Realtime_PC()
     id = 0
-    while True:
-        realtime_PC.get_now_pcd_from_camera()
-        realtime_PC.vis_PC()
-        # realtime_PC.save_pc(save_dir="data/saved_pc/blue _short", file_name=f"blue _short_{id}.ply")
+    # realtime_PC.multi_rs.create_window_and_capture_data(save_dir=Path("/home/shaol/data/zhoujx/Obj_mesh_dir/Box_02"))
+    # while True:
+    realtime_PC.get_now_pcd_from_camera()
+    #     realtime_PC.vis_PC()
+    #     realtime_PC.save_pc(save_dir="/home/shaol/data/zhoujx/Obj_mesh_dir/Box_02", file_name=f"Box_02.ply")
 
-        # ### load the point cloud from the saved file
-        # # realtime_PC.load_pc(load_dir="data/saved_pc", file_name="long_pants_v.ply")
-        # realtime_PC.load_pc(load_dir="data/saved_pc/long_pants_1", file_name=f"long_pant_{id}.ply")
-        # realtime_PC.vis_PC()
-        # if id == 0:
-        #     time.sleep(5)
-        # time.sleep(1)
-        # id += 1
+    ### load the point cloud from the saved file
+    realtime_PC.load_pc(load_dir="data/saved_pc", file_name="long_pants_v.ply")
+    pcs=realtime_PC.load_pc(load_dir="/home/shaol/data/zhoujx/Obj_mesh_dir/Box_02", file_name=f"Box_02.ply")
+    # realtime_PC.vis_PC()
+    u, v = 600, 600
 
-    # prompt_drawer = SAMPromptDrawer(window_name="Prompt Drawer", screen_scale=2.0, sam_checkpoint=SAM_PATH, device="cuda", model_type=SAM_TYPE)
-    prompt_drawer = SAM2PromptDrawer(window_name="Prompt Drawer",
-                                     screen_scale=2.0,
-                                     sam_checkpoint=SAM_PATH,
-                                     device="cuda",
-                                     model_type=SAM_TYPE)
-    # prompt_drawer = SAMAutoDrawer(window_name="Prompt Drawer", screen_scale=2.0, sam_checkpoint=SAM_PATH, device="cuda", model_type=SAM_TYPE)
-
-    arm_right_cam_serial = "241122074374"
-    camera_serial_nums = [arm_right_cam_serial]
-    multi_rs = MultiRealsense(camera_serial_nums)
-    arm_right_cam_K_path = Path("3rdparty/xarm6/data/camera/mounted_white/K.npy")
-    arm_right_cam_K = np.load(arm_right_cam_K_path)
-    arm_right_cam_X_BaseCamera_path = Path(
-        "3rdparty/xarm6/data/camera/mounted_white/0922_excalib_capture00/optimized_X_BaseCamera.npy")
-    arm_right_cam_X_BaseCamera = np.load(arm_right_cam_X_BaseCamera_path)
-    multi_rs.set_intrinsics(0, arm_right_cam_K[0, 0], arm_right_cam_K[1, 1], arm_right_cam_K[0, 2], arm_right_cam_K[1,
-                                                                                                                    2])
-    camera_wxyzs = [
-        R.from_matrix(arm_right_cam_X_BaseCamera[:3, :3]).as_quat()[[3, 0, 1, 2]],
-    ]
-    camera_positions = [arm_right_cam_X_BaseCamera[:3, 3]]
-    X_BaseCamera_list = [arm_right_cam_X_BaseCamera]
-
-    sv = viser.ViserServer(port=8080)
-
-    for _ in range(50):
-        multi_rs.getCurrentData()
-    if_init = True
-
-    while True:
-        rtr_dict_list = multi_rs.getCurrentData()
-        for camera_idx in range(len(rtr_dict_list)):
-            rtr_dict = rtr_dict_list[camera_idx]
-
-            rgb = rtr_dict["rgb"]
-            depth = (rtr_dict["depth"].astype(np.float32)).astype(np.float32)
-            # pc_o3d = rtr_dict["pointcloud_o3d"]
-
-            prompt_drawer.reset()
-            if if_init:
-                mask_np = prompt_drawer.run(rgb)  # (720, 1280)
-                if_init = False
-            else:
-                mask_np = prompt_drawer.track(rgb)
-
-            h, w = mask_np.shape[-2:]
-
-            masked_pc_o3d = get_masked_pointcloud(rgb, depth, mask_np.reshape(h, w),
-                                                  multi_rs.camera_data[camera_idx]["pinhole_camera_intrinsic"])
-            X_BaseCamera = X_BaseCamera_list[camera_idx]
-            rs_pc_in_C_np = np.asarray(masked_pc_o3d.points)
-            rs_pc_in_B = X_BaseCamera @ np.vstack([rs_pc_in_C_np.T, np.ones(rs_pc_in_C_np.shape[0])])
-            rs_pc_in_B = rs_pc_in_B[:3].T
-            masked_pc_o3d_W = o3d.geometry.PointCloud()
-            masked_pc_o3d_W.points = o3d.utility.Vector3dVector(rs_pc_in_B)
-            masked_pc_o3d_W.colors = masked_pc_o3d.colors
-
-        sv.scene.add_point_cloud('object_pc',
-                                 np.asarray(masked_pc_o3d_W.points),
-                                 colors=np.asarray(masked_pc_o3d_W.colors),
-                                 point_size=0.003,
-                                 point_shape="circle")
+    try:
+        # 可视化 2D 和 3D 点
+        realtime_PC.visualize_2d_and_3d(u, v)
+    except ValueError as e:
+        print(e)
